ef4ff568817506d5569df3553e7095ac6e6747f9
siobhan-mei <siobhanmeic@gmail.com>
2026-08-23 12:25:40 +0800

    Rename config files (more descriptive and less generic)

--- resources/js/config/editorPanelAssets.js
+++ resources/js/config/editorPanelAssets.js
@@ -3,7 +3,6 @@ export const EDITOR_FRAMES = [
     { id: 'editor-frame-2', thumbnail: null, name: 'Second'},
     { id: 'editor-frame-3', thumbnail: null, name: 'Third'},
 ];
-
 export const EDITOR_STICKERS = Array.from({ length: 10 }, (_, i) => ({
     id: `sticker-${i + 1}`,
     thumbnail: null,
